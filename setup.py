8cba7fbaacb77aa804fd3193e4a244728ec3b46c
Luke Pavksy <85752004+lukedevj@users.noreply.github.com>
2021-07-30 19:26:03 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='notify', version='0.1.0-beta',
     py_modules=['notify'],
     install_requires=[
-        'click==8.0.1',
-        'toml==0.10.2'
+        'click',
+        'toml'
     ],
     entry_points='''
         [console_scripts]
